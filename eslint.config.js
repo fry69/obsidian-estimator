7e3199c83adea9d48167f0f14b1bb88389a71d28
fry69 <142489379+fry69@users.noreply.github.com>
2025-10-21 10:31:54 +0200

    feat: WIP snapshot 3

--- eslint.config.js
+++ eslint.config.js
@@ -6,9 +6,10 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'worker-configuration.d.ts']),
   {
     files: ['**/*.{ts,tsx}'],
+
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
